89b37a139aaba7c262b1f272685db57523ca36f4
Abdelbassit-issa <abdelbassit6877@gmail.com>
2025-03-10 04:17:13 +0300

    ...................

---
+++ lib/repos/connections/.keys.dart
@@ -0,0 +1,13 @@
+
+
+const String JWT_SECRET = "SfJhqFDykkh5MXJUcpinTawkI8gg4rjrfOrnXeV4";
+const String ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
+const String SERVICE_ROLE_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogInNlcnZpY2Vfcm9sZSIsCiAgImlzcyI6ICJzdXBhYmFzZSIsCiAgImlhdCI6IDE3MjY5ODg0MDAsCiAgImV4cCI6IDE4ODQ3NTQ4MDAKfQ.7mx7qkKQGmJWiMdxeFuY3NkVaTKat4FiNW6pm8RKLfw";
+const piAddress = "localhost";
+const String rEST = "http://$piAddress:8000/rest/v1/";
+const String mainURL = "http://$piAddress:8000";
+const String auth = "http://$piAddress:8000/auth/v1/";
+const String storage = "http://$piAddress:8000/storage/v1/";
+const String realtime = "http://$piAddress:8000/realtime/v1/";
